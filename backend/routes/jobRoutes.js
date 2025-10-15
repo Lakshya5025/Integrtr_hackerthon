de6fae0cb45d869e3c9ac6bbd1cea69b85e5752b
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-15 14:40:49 +0530

    create job router

---
+++ backend/routes/jobRoutes.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const {
+    getAllPublicJobs,
+    getJobsForMyNgo,
+    createJob,
+    updateJob,
+    deleteJob,
+} = require('../controllers/jobController');
+
+const { protect, authorize } = require('../middleware/auth');
+
+const router = express.Router();
+
+// Public Route - anyone can view jobs
+router.get('/public', getAllPublicJobs);
+
+// --- Protected Admin Routes Below ---
+router.use(protect);
+router.use(authorize('admin'));
+
+router.route('/')
+    .get(getJobsForMyNgo)
+    .post(createJob);
+
+router.route('/:id')
+    .put(updateJob)
+    .delete(deleteJob);
+
+module.exports = router;
