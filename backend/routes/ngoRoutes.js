b6ce793bf45d893a114ea72823fc89ffe7d6fd6c
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-15 14:24:38 +0530

    create ngo router

---
+++ backend/routes/ngoRoutes.js
@@ -0,0 +1,15 @@
+// routes/ngoRoutes.js
+
+const express = require('express');
+const { registerNgo } = require('../controllers/ngoController');
+const { protect } = require('../middleware/auth'); // Import the middleware
+
+const router = express.Router();
+
+// Any route defined here is prefixed with /api/ngos
+
+// We apply the 'protect' middleware to this route.
+// A user must be logged in to access it.
+router.post('/register', protect, registerNgo);
+
+module.exports = router;
